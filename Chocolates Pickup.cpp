f1e56fc5f11ce75e0a33cf0e662d48e522f0a7f2
RafayethRafi <87166899+RafayethRafi@users.noreply.github.com>
2023-05-11 21:45:07 +0600

    Create Chocolates Pickup.cpp
    https://practice.geeksforgeeks.org/problems/chocolates-pickup/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=chocolates-pickup

---
+++ Chocolates Pickup.cpp
@@ -0,0 +1,139 @@
+// Memoization
+
+class Solution {
+  public:
+  
+    int solve(int i,int j1,int j2,int r,int c, vector<vector<int>>& grid, vector<vector<vector<int>>> &dp ){
+        if(j1<0 || j2<0 || j1>=c || j2>=c) return -1e8;
+        if(i == r-1)
+        {
+            if( j1 == j2) return grid[i][j1];
+            else return grid[i][j1] + grid[i][j2];
+        }
+        
+        if(dp[i][j1][j2] != -1) return dp[i][j1][j2];
+        int maxi = -1e8;
+        for(int dj1= -1;dj1<=1; dj1++)
+        {
+            for(int dj2= -1;dj2<=1; dj2++)
+            {
+                int value = 0;
+                if(j1==j2) value = grid[i][j1];
+                else value = grid[i][j1] + grid[i][j2];
+                
+                value += solve(i+1,j1+dj1,j2+dj2,r,c,grid,dp);
+                
+                maxi = max(maxi, value);
+            }
+        }
+        
+        return dp[i][j1][j2] = maxi;
+    }
+    int solve(int n, int m, vector<vector<int>>& grid) {
+        
+        vector<vector<vector<int>>> dp(n,vector<vector<int>> (m,vector<int> (m,-1)));
+        
+        return solve(0,0,m-1,n,m,grid,dp);
+    }
+};
+
+// TABULATION
+
+class Solution {
+  public:
+
+    int solve(int n, int m, vector<vector<int>>& grid) {
+        
+        vector < vector < vector < int >>> dp(n, vector < vector < int >> (m, 
+  vector < int > (m, 0)));
+        
+        for(int j1= 0;j1<m;j1++)
+        {
+            for(int j2=0;j2<m;j2++){
+                if(j1==j2) dp[n-1][j1][j2] = grid[n-1][j1];
+                else dp[n-1][j1][j2] = grid[n-1][j1] + grid[n-1][j2];
+            }
+        }
+        
+        for(int i = n-2;i>=0;i--)
+        {
+        for(int j1= 0;j1<m;j1++)
+        {
+        for(int j2=0;j2<m;j2++){
+                int maxi = -1e8;
+        for(int dj1= -1;dj1<=1; dj1++)
+        {
+            for(int dj2= -1;dj2<=1; dj2++)
+            {
+                int value = 0;
+                if(j1==j2) value = grid[i][j1];
+                else value = grid[i][j1] + grid[i][j2];
+                
+                if ((j1 + dj1 < 0 || j1 + dj1 >= m) ||
+              (j2 + dj2 < 0 || j2 + dj2 >= m)) value += -1e8;
+              
+              else value += dp[i+1][j1+dj1][j2+dj2];
+                
+                maxi = max(maxi, value);
+            }
+        }
+        
+        dp[i][j1][j2] = maxi;
+            }
+        }
+        }
+        
+        return dp[0][0][m-1];
+    }
+};
+
+
+//SPACE OPTIMIZATION
+
+class Solution {
+  public:
+
+    int solve(int n, int m, vector<vector<int>>& grid) {
+        
+        vector<vector<int>> front(m, vector<int>(m)) , cur(m, vector<int>(m));
+        
+        for(int j1= 0;j1<m;j1++)
+        {
+            for(int j2=0;j2<m;j2++){
+                if(j1==j2) front[j1][j2] = grid[n-1][j1];
+                else front[j1][j2] = grid[n-1][j1] + grid[n-1][j2];
+            }
+        }
+        
+        for(int i = n-2;i>=0;i--)
+        {
+        for(int j1= 0;j1<m;j1++)
+        {
+        for(int j2=0;j2<m;j2++){
+                int maxi = -1e8;
+        for(int dj1= -1;dj1<=1; dj1++)
+        {
+            for(int dj2= -1;dj2<=1; dj2++)
+            {
+                int value = 0;
+                if(j1==j2) value = grid[i][j1];
+                else value = grid[i][j1] + grid[i][j2];
+                
+                if ((j1 + dj1 < 0 || j1 + dj1 >= m) ||
+              (j2 + dj2 < 0 || j2 + dj2 >= m)) value += -1e8;
+              
+              else value += front[j1+dj1][j2+dj2];
+                
+                maxi = max(maxi, value);
+            }
+        }
+        
+        cur[j1][j2] = maxi;
+            }
+        }
+        front = cur;
+        }
+        
+        return front[0][m-1];
+    }
+};
